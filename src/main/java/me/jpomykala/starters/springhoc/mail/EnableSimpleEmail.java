c9bdaa64959bab6b30c05aedd869051c716fb533
jpomykala <evelan3094@gmail.com>
2018-10-16 19:44:21 +0200

    add enabling simple emailing by annotation

---
+++ src/main/java/me/jpomykala/starters/springhoc/mail/EnableSimpleEmail.java
@@ -0,0 +1,13 @@
+package me.jpomykala.starters.springhoc.mail;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({SpringHocMailAutoConfiguration.class})
+public @interface EnableSimpleEmail {
+}
--- src/main/java/me/jpomykala/starters/springhoc/mail/SpringHocMailAutoConfiguration.java
+++ src/main/java/me/jpomykala/starters/springhoc/mail/SpringHocMailAutoConfiguration.java
@@ -2,8 +2,6 @@ package me.jpomykala.starters.springhoc.mail;
 
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.SearchStrategy;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +17,6 @@ public class SpringHocMailAutoConfiguration {
   private AmazonSimpleEmailService amazonSimpleEmailService;
 
   @Bean
-  @ConditionalOnMissingBean(search = SearchStrategy.ALL)
   public SpringHocMailService mailSendingService() {
     return new SpringHocMailService(configuration, amazonSimpleEmailService);
   }
