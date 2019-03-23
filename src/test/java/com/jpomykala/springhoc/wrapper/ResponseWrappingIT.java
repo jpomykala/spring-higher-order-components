5c4f9e2f22eb30d3971c9dec0bfbc62d3bea07a8
jpomykala <evelan3094@gmail.com>
2019-03-23 23:52:36 +0100

    Add response wrapping integration tests

---
+++ src/test/java/com/jpomykala/springhoc/wrapper/ResponseWrappingIT.java
@@ -0,0 +1,75 @@
+package com.jpomykala.springhoc.wrapper;
+
+import com.amazonaws.util.IOUtils;
+import com.jayway.jsonpath.JsonPath;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {TestWrapperApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ResponseWrappingIT {
+  @LocalServerPort
+  private int serverPort;
+
+  private HttpClient client = HttpClientBuilder.create().build();
+
+  @Test
+  public void shouldWrapEntityResponseWhenConfigured() throws Exception {
+    //given
+    String url = "http://localhost:" + serverPort + "/echo";
+    HttpGet request = new HttpGet(url);
+
+    //when
+    HttpResponse response = client.execute(request);
+
+    //then
+    String json = IOUtils.toString(response.getEntity().getContent());
+    String firstName = JsonPath.read(json, "$.data.firstName");
+    Assertions.assertThat(firstName).isEqualTo("Jakub");
+
+    String lastName = JsonPath.read(json, "$.data.lastName");
+    Assertions.assertThat(lastName).isEqualTo("Pomykała");
+
+    String message = JsonPath.read(json, "$.msg");
+    Assertions.assertThat(message).isEqualTo("OK");
+
+    int status = JsonPath.read(json, "$.status");
+    Assertions.assertThat(status).isEqualTo(200);
+  }
+
+  @Test
+  public void shouldWrapPageResponseWhenConfigured() throws Exception {
+    //given
+    String url = "http://localhost:" + serverPort + "/paged-echo";
+    HttpGet request = new HttpGet(url);
+
+    //when
+    HttpResponse response = client.execute(request);
+
+    //then
+    String json = IOUtils.toString(response.getEntity().getContent());
+    String firstName = JsonPath.read(json, "$.data[0].firstName");
+    Assertions.assertThat(firstName).isEqualTo("Jakub");
+
+    String lastName = JsonPath.read(json, "$.data[0].lastName");
+    Assertions.assertThat(lastName).isEqualTo("Pomykała");
+
+    String message = JsonPath.read(json, "$.msg");
+    Assertions.assertThat(message).isEqualTo("OK");
+
+    int status = JsonPath.read(json, "$.status");
+    Assertions.assertThat(status).isEqualTo(200);
+
+    int totalElements = JsonPath.read(json, "$.pageDetails.totalElements");
+    Assertions.assertThat(totalElements).isEqualTo(2);
+  }
+
+}
---
+++ src/test/java/com/jpomykala/springhoc/wrapper/TestWrapperApplication.java
@@ -0,0 +1,67 @@
+package com.jpomykala.springhoc.wrapper;
+
+import org.assertj.core.util.Lists;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication(scanBasePackages = {"com.jpomykala.springhoc.wrapper"})
+@RestController
+public class TestWrapperApplication extends SpringBootServletInitializer {
+  public static void main(String[] args) {
+    SpringApplication.run(TestWrapperApplication.class, args);
+  }
+
+  @GetMapping("/echo")
+  public MyPojo echo() {
+    return new MyPojo()
+            .setFirstName("Jakub")
+            .setLastName("Pomykała");
+  }
+
+  @GetMapping("/paged-echo")
+  public Page<MyPojo> pageEcho() {
+    MyPojo jakub = new MyPojo()
+            .setFirstName("Jakub")
+            .setLastName("Pomykała");
+
+    MyPojo nieMaJajek = new MyPojo()
+            .setFirstName("Michał")
+            .setLastName("Białek");
+
+    List<MyPojo> pojoList = Lists.newArrayList(jakub, nieMaJajek);
+    int totalElements = pojoList.size();
+    return new PageImpl<>(pojoList, Pageable.unpaged(), totalElements);
+  }
+
+
+  public class MyPojo {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+      return firstName;
+    }
+
+    public MyPojo setFirstName(String firstName) {
+      this.firstName = firstName;
+      return this;
+    }
+
+    public String getLastName() {
+      return lastName;
+    }
+
+    public MyPojo setLastName(String lastName) {
+      this.lastName = lastName;
+      return this;
+    }
+  }
+}
